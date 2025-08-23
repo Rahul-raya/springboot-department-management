c5883155cc34f69e41ef8686f8563200ab6e53e4
Rahul-raya <rahul.22bce8855@vitapstudent.ac.in>
2025-08-23 19:14:03 +0530

    Add QA, Dev, and Prod profiles to application.yaml

--- src/main/java/com/example/Springbootpractice/Controller/HelloController.java
+++ src/main/java/com/example/Springbootpractice/Controller/HelloController.java
@@ -4,10 +4,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.beans.factory.annotation.Value;
+
+
 @RestController
 public class HelloController {
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String sayHello() {
-        return "Hello, World!aaa";
+        return welcomeMessage;
     }   
 }
